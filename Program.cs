3948b1eba8f23207e3cafc19eb04370afbadc8b7
Aishwarya C <chan.aishwarya@gmail.com>
2024-01-30 15:46:02 +0530

    TotalWorkingHours

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uc3
+namespace UC6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int parttime = 4;
-            int wage = 20;
-            int fulltime = 8;
+            int totalNoOfDays = 20;
+            int totalNoOfHrs = 100;
+            int wageRate = 20;
+            int daycount = 0;
+            int hrscount = 0;
 
-            Console.WriteLine("Part time employee : " + (parttime * wage));
-            Console.WriteLine("By adding both full time and part time  : " + (parttime * wage + fulltime * wage));
+            while (daycount <= totalNoOfDays && hrscount <= totalNoOfHrs)
+            {
+                daycount += 1;
+                hrscount += 8;
+                Console.WriteLine($"Your salary at end of day {daycount} is " + (Convert.ToInt32(hrscount * wageRate)));
+
+
+            }
             Console.ReadLine();
         }
     }
