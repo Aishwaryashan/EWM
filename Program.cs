52b76106d4690a477121423df8136ffbfe67ece8
Aishwarya C <chan.aishwarya@gmail.com>
2024-01-30 15:10:42 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computaion Program.");
+
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+
+            if (attendance == 1)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
